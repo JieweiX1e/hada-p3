472b1ac12f2a246fb352c1e338ffb8dcc03ef71c
Lyfjkrz <vicentemanuelruizdelope@gmail.com>
2026-03-22 21:06:05 +0100

    55178369N Formatted Site1.Master

--- ProWeb/Site1.Master.designer.cs
+++ ProWeb/Site1.Master.designer.cs
@@ -31,12 +31,21 @@ namespace ProWeb {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
         /// <summary>
-        /// Control ContentPlaceHolder1.
+        /// Control Algo.
         /// </summary>
         /// <remarks>
         /// Campo generado automáticamente.
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
+        protected global::System.Web.UI.WebControls.Label Algo;
+
+        /// <summary>
+        /// Control cph1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder cph1;
     }
 }
