6ddbaa44a68cbbdbaf3aacdd58b91e64ea97ae2a
Jiewei Xie <jieweixie43@gmail.com>
2026-03-22 15:23:18 +0100

    X8901823H CADProducts file methods and properties added to be completed

--- library/CADProduct.cs
+++ library/CADProduct.cs
@@ -8,6 +8,41 @@ namespace library
 {
     public class CADProduct
     {
+        private string constring;
+        public CADProduct() { 
+
+        }
+
+        public bool Create(ENProduct en) { 
+        
+        }
+
+        public bool Update(ENProduct en) { 
+        
+        }
+
+        public bool Delete(ENProduct en) { 
+        
+        }
+
+        public bool Read(ENProduct en) { 
+        
+        
+        }
+
+        public bool ReadFirst(ENProduct en) { 
+        
+        }
+
+        public bool ReadNext(ENProduct en) { 
+        
+        }
+
+        public bool ReadPrev(ENProduct en) { 
+        
+        
+        }
+
 
     }
 }
--- library/ENProduct.cs
+++ library/ENProduct.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace library
 {
-    internal class ENProducts
+    public class ENProduct
     {
+
     }
 }
